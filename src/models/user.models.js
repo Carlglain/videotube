6dc4563ce8fe557633546f0ecb3b6e48d5e0de3b
Carlstorm <carlglainnyuysemo@gmail.com>
2025-10-16 05:40:37 +0100

    feat: i learnt: password hashing with bycrypt and testing whether the password is correct, creating methods for getting the access and refresh tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt, { sign } from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
@@ -50,4 +51,33 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+userSchema.pre("save", async function (next) {
+  if (!this.modified(this.password)) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  // short lived access token
+  return jwt.sign(
+    {
+      _id: this._id, //this is what is generally required.
+      usernamename: this.username, //optional and you call take more info like below
+      // fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    REFRESH_TOKEN_SECRET,
+    { expiresIn: REFRESH_TOKEN_EXPIRY }
+  );
+};
 export const User = model("User", userSchema);
